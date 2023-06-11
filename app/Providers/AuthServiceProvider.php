3bf0a87b3a2f08fe7923d9587316c9a7d5f43dba
Nafies Luthfi <nafiesl@gmail.com>
2023-06-11 11:23:47 +0800

    feat: Laravel 10.x upgrade
    Run php artisan passport:install

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,10 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        // Register passport routes
-        Passport::routes(function ($router) {
-            $router->forAccessTokens();
-        });
     }
 }
