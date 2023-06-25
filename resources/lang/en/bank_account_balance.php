d9277089c79f91a7b3b736523577a090007abd93
Nafies Luthfi <nafiesl@gmail.com>
2023-06-25 17:06:13 +0800

    feat: Add missing bank account balance lang for english

---
+++ resources/lang/en/bank_account_balance.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    // Labels
+    'bank_account_balance' => 'Balance History',
+    'list' => 'Balance History List',
+    'empty' => 'Balance History is empty.',
+
+    // Actions
+    'create' => 'Add new Balance History',
+    'created' => 'Balance History has been added.',
+    'show' => 'View Balance History Detail',
+    'edit' => 'Edit Balance History',
+    'update' => 'Update Balance History',
+    'updated' => 'Balance History updated.',
+    'delete' => 'Delete Balance History',
+    'delete_confirm' => 'Are you sure to delete this Balance History?',
+    'deleted' => 'Balance History deleted.',
+    'undeleted' => 'Balance History not deleted.',
+    'undeleteable' => 'Balance History cannot be deleted.',
+
+    // Attributes
+    'date' => 'Per Date',
+    'amount' => 'Balance',
+];
