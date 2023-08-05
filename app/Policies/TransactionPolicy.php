35d33cf68c64c78289c647c91dae804e6d316ec5
Nafies Luthfi <nafiesl@gmail.com>
2023-08-05 23:03:18 +0800

    feat: Update transaction policy

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -10,55 +10,23 @@ class TransactionPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Transaction  $transaction
-     * @return mixed
-     */
-    public function view(User $user, Transaction $transaction)
-    {
-        // Update $user authorization to view $transaction here.
-        return $user->id == $transaction->creator_id;
-    }
-
-    /**
-     * Determine whether the user can create projects.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Transaction  $transaction
-     * @return mixed
-     */
     public function create(User $user, Transaction $transaction)
     {
-        // Update $user authorization to create $transaction here.
+        return in_array($user->role_id, [User::ROLE_ADMIN, User::ROLE_FINANCE]);
+    }
+
+    public function view(User $user, Transaction $transaction)
+    {
         return true;
     }
 
-    /**
-     * Determine whether the user can update the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Transaction  $transaction
-     * @return mixed
-     */
     public function update(User $user, Transaction $transaction)
     {
-        // Update $user authorization to update $transaction here.
-        return $user->id == $transaction->creator_id;
+        return in_array($user->role_id, [User::ROLE_ADMIN, User::ROLE_FINANCE]);
     }
 
-    /**
-     * Determine whether the user can delete the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Transaction  $transaction
-     * @return mixed
-     */
     public function delete(User $user, Transaction $transaction)
     {
-        // Update $user authorization to delete $transaction here.
-        return $user->id == $transaction->creator_id;
+        return in_array($user->role_id, [User::ROLE_ADMIN, User::ROLE_FINANCE]);
     }
 }
--- tests/Unit/Policies/TransactionPolicyTest.php
+++ tests/Unit/Policies/TransactionPolicyTest.php
@@ -14,39 +14,67 @@ class TransactionPolicyTest extends TestCase
     public function user_can_create_transaction()
     {
         $admin = $this->createUser('admin');
+        $chairman = $this->createUser('chairman');
+        $secretary = $this->createUser('secretary');
+        $finance = $this->createUser('finance');
+
         $this->assertTrue($admin->can('create', new Transaction));
+        $this->assertFalse($chairman->can('create', new Transaction));
+        $this->assertFalse($secretary->can('create', new Transaction));
+        $this->assertTrue($finance->can('create', new Transaction));
     }
 
     /** @test */
-    public function user_can_only_view_their_own_transaction()
+    public function user_can_see_transaction_details()
     {
         $admin = $this->createUser('admin');
+        $chairman = $this->createUser('chairman');
+        $secretary = $this->createUser('secretary');
+        $finance = $this->createUser('finance');
+
         $transaction = factory(Transaction::class)->create(['creator_id' => $admin->id]);
         $othersTransaction = factory(Transaction::class)->create();
 
         $this->assertTrue($admin->can('view', $transaction));
-        $this->assertFalse($admin->can('view', $othersTransaction));
+        $this->assertTrue($admin->can('view', $othersTransaction));
+        $this->assertTrue($chairman->can('view', $transaction));
+        $this->assertTrue($secretary->can('view', $transaction));
+        $this->assertTrue($finance->can('view', $transaction));
     }
 
     /** @test */
-    public function user_can_only_update_their_own_transaction()
+    public function admin_and_finance_can_update_transaction()
     {
         $admin = $this->createUser('admin');
+        $chairman = $this->createUser('chairman');
+        $secretary = $this->createUser('secretary');
+        $finance = $this->createUser('finance');
+
         $transaction = factory(Transaction::class)->create(['creator_id' => $admin->id]);
         $othersTransaction = factory(Transaction::class)->create();
 
         $this->assertTrue($admin->can('update', $transaction));
-        $this->assertFalse($admin->can('update', $othersTransaction));
+        $this->assertTrue($admin->can('update', $othersTransaction));
+        $this->assertFalse($chairman->can('update', $transaction));
+        $this->assertFalse($secretary->can('update', $transaction));
+        $this->assertTrue($finance->can('update', $transaction));
     }
 
     /** @test */
-    public function user_can_only_delete_their_own_transaction()
+    public function admin_and_finance_can_delete_transaction()
     {
         $admin = $this->createUser('admin');
+        $chairman = $this->createUser('chairman');
+        $secretary = $this->createUser('secretary');
+        $finance = $this->createUser('finance');
+
         $transaction = factory(Transaction::class)->create(['creator_id' => $admin->id]);
         $othersTransaction = factory(Transaction::class)->create();
 
         $this->assertTrue($admin->can('delete', $transaction));
-        $this->assertFalse($admin->can('delete', $othersTransaction));
+        $this->assertTrue($admin->can('delete', $othersTransaction));
+        $this->assertFalse($chairman->can('delete', $transaction));
+        $this->assertFalse($secretary->can('delete', $transaction));
+        $this->assertTrue($finance->can('delete', $transaction));
     }
 }
