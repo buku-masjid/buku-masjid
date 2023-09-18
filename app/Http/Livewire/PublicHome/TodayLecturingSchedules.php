fe92d7c958490a65bfff7a03befd6d3f126a6210
Muhammad Irwan Andriawan <irwan@andriawan.com>
2023-09-18 04:13:24 +0000

    chore: apply code suggestion form review

--- app/Http/Livewire/PublicHome/TodayLecturingSchedules.php
+++ app/Http/Livewire/PublicHome/TodayLecturingSchedules.php
@@ -8,33 +8,9 @@ use Livewire\Component;
 
 class TodayLecturingSchedules extends Component
 {
-    public $header;
-    public $detailTextButton;
-    public $intervalCarousel;
-    public $lecturingSchedules = [];
-    public $audienceCodes = [];
-    public $linkDetailSchedule;
-    public $isFriday;
     public $audienceFriday;
     public $lecturerName;
-
-    public function getAudienceCodeList(): array
-    {
-        return [
-            LecturingSchedule::AUDIENCE_FRIDAY => __('lecturing_schedule.audience_' . LecturingSchedule::AUDIENCE_FRIDAY),
-            LecturingSchedule::AUDIENCE_PUBLIC => __('lecturing_schedule.audience_' . LecturingSchedule::AUDIENCE_PUBLIC),
-            LecturingSchedule::AUDIENCE_MUSLIMAH => __('lecturing_schedule.audience_' . LecturingSchedule::AUDIENCE_MUSLIMAH),
-        ];
-    }
-
-    public function getHeader(): array
-    {
-        return [
-            LecturingSchedule::AUDIENCE_FRIDAY => __('lecturing_schedule.friday_lecturing_schedule') . ' ' . __('time.today'),
-            LecturingSchedule::AUDIENCE_PUBLIC => __('lecturing_schedule.lecturing_schedule') . ' ' . __('time.today'),
-            LecturingSchedule::AUDIENCE_MUSLIMAH => __('lecturing_schedule.lecturing_schedule') . ' ' . __('time.today')
-        ];
-    }
+    public $lecturingSchedules = [];
 
     public function getLecturerName(): array
     {
@@ -45,34 +21,14 @@ class TodayLecturingSchedules extends Component
         ];
     }
 
-    public function today()
+    public function mount()
     {
         $lecturingScheduleQuery = LecturingSchedule::query();
         $lecturingScheduleQuery->where('date', Carbon::today()->format('Y-m-d'));
         $lecturingScheduleQuery->orderBy('date')->orderBy('start_time');
-        $this->lecturingSchedules = $lecturingScheduleQuery->get()
-            ->filter(function ($lecturingSchedule) {
-                if (
-                    $lecturingSchedule->audience_code === LecturingSchedule::AUDIENCE_FRIDAY &&
-                    !$this->isFriday
-                ) {
-                    return false;
-                }
-                return true;
-            });
-        $this->audienceCodes = $this->getAudienceCodeList();
+        $this->lecturingSchedules = $lecturingScheduleQuery->get();
         $this->lecturerName = $this->getLecturerName();
-    }
-
-    public function mount()
-    {
-        $this->header = $this->getHeader();
-        $this->detailTextButton = __('app.show');
-        $this->intervalCarousel = "5000";
-        $this->linkDetailSchedule = route('public_schedules.index');
-        $this->isFriday = strtolower(Carbon::today()->format('l')) === LecturingSchedule::AUDIENCE_FRIDAY;
         $this->audienceFriday = LecturingSchedule::AUDIENCE_FRIDAY;
-        $this->today();
     }
 
     public function render()
--- resources/views/livewire/public_home/today_lecturing_schedules.blade.php
+++ resources/views/livewire/public_home/today_lecturing_schedules.blade.php
@@ -1,50 +1,37 @@
-<div class="card">
-    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            @if ($lecturingSchedules->isEmpty())
-            <div class="carousel-item active" data-interval="1000">
-                <div class="card-header">
-                    <h3 class="card-title flex-grow-1">{{ $header['public'] }}</h3>
-                    <a class="btn btn-sm btn-success" href="{{ $linkDetailSchedule }}" role="button">{{ $detailTextButton }}</a>
-                </div>
-                <div class="card-body">
-                    <p class>
-                        {{ __('lecturing_schedule.today_empty') }}
-                    </p>
-                </div>
-            </div>
-            @else
-            @foreach($lecturingSchedules as $index => $lecturingSchedule)
-            <div class="carousel-item {{ $isFriday && $lecturingSchedule->audience_code === $audienceFriday ? 'active' : ($index === 0 && !$isFriday ? 'active' : '') }}" data-interval="{{ $intervalCarousel }}">
-                <div class="card-header">
-                    <h3 class="card-title flex-grow-1">{{ $header[$lecturingSchedule->audience_code] }}</h3>
-                    <a class="btn btn-sm btn-success" href="{{ $linkDetailSchedule }}" role="button">{{ $detailTextButton }}</a>
-                </div>
-                <table class="table-sm mb-0 table-bordered w-100">
-                    <tbody>
-                        <tr>
-                            <td class="col-4">{!! config('lecturing.emoji.lecturing') !!} {{ __('lecturing_schedule.lecturing') }}</td>
-                            <td><strong>{{ $lecturingSchedule->day_name }}, {{ $lecturingSchedule->audience_code === $audienceFriday ? $lecturingSchedule->full_date : $lecturingSchedule->time_text }}</strong></td>
-                        </tr>
-                        @if ($lecturingSchedule->audience_code !== $audienceFriday)
-                        <tr>
-                            <td>{!! config('lecturing.emoji.date') !!} {{ __('time.date') }}</td>
-                            <td>{{ $lecturingSchedule->full_date }}</td>
-                        </tr>
-                        @endif
-                        <tr>
-                            <td>{!! config('lecturing.emoji.time') !!} {{ __('lecturing_schedule.time') }}</td>
-                            <td>{{ $lecturingSchedule->audience_code === $audienceFriday ? $lecturingSchedule->start_time : $lecturingSchedule->time }}</td>
-                        </tr>
-                        <tr>
-                            <td>{!! config('lecturing.emoji.lecturer') !!} {{ $lecturerName[$lecturingSchedule->audience_code] }}</td>
-                            <td>{{ $lecturingSchedule->lecturer_name }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            @endforeach
-            @endif
-        </div>
+<div class="page-header">
+    <h3 class="page-title">{{ __('lecturing_schedule.public_schedule') }} {{ __('time.today') }}</h3>
+    <div class="page-options d-flex">
+        @if (!$lecturingSchedules->isEmpty())
+        <a class="btn btn-sm btn-success" href="{{ route('public_schedules.index') }}" role="button">{{ __('app.show') }}</a>
+        @endif
     </div>
 </div>
+@forelse($lecturingSchedules as $index => $lecturingSchedule)
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">
+            {{ __('lecturing_schedule.audience_'.$lecturingSchedule->audience_code) }}
+        </h3>
+    </div>
+    <table class="table table-sm mb-0">
+        <tbody>
+            @if ($lecturingSchedule->audience_code != $audienceFriday)
+            <tr>
+                <td class="col-4">{!! config('lecturing.emoji.lecturing') !!} {{ __('lecturing_schedule.lecturing') }}</td>
+                <td><strong>{{ $lecturingSchedule->day_name }}, {{ $lecturingSchedule->time_text }}</strong></td>
+            </tr>
+            @endif
+            <tr>
+                <td>{!! config('lecturing.emoji.time') !!} {{ __('lecturing_schedule.time') }}</td>
+                <td>{{ $lecturingSchedule->audience_code === $audienceFriday ? $lecturingSchedule->start_time : $lecturingSchedule->time }}</td>
+            </tr>
+            <tr>
+                <td>{!! config('lecturing.emoji.lecturer') !!} {{ $lecturerName[$lecturingSchedule->audience_code] }}</td>
+                <td>{{ $lecturingSchedule->lecturer_name }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@empty
+{{ __('lecturing_schedule.today_empty') }}.
+@endforelse
