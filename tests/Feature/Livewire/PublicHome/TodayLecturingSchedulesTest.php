95c98c55b857d17809ebc5161d2ec26be02fd244
Muhammad Irwan Andriawan <irwan@andriawan.com>
2023-09-18 04:13:43 +0000

    test: improve test

--- tests/Feature/Livewire/PublicHome/TodayLecturingSchedulesTest.php
+++ tests/Feature/Livewire/PublicHome/TodayLecturingSchedulesTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature\Liveware\PublicHome;
 
-use App\Http\Livewire\TodayLecturerCard;
+use App\Http\Livewire\PublicHome\TodayLecturingSchedules;
 use App\Models\LecturingSchedule;
 use App\User;
 use Carbon\Carbon;
@@ -17,10 +17,8 @@ class TodayLecturingSchedulesTest extends TestCase
     /** @test */
     public function user_can_see_empty_lecturing_today_card()
     {
-        $user = factory(User::class)->create(['name' => 'Nama Member', 'email' => 'email@mail.com']);
-        $this->loginAsUser();
         $this->visit('/');
-        Livewire::test(TodayLecturerCard::class)
+        Livewire::test(TodayLecturingSchedules::class)
             ->assertCount('lecturingSchedules', 0)
             ->assertSee(__('lecturing_schedule.today_empty'));
     }
@@ -28,46 +26,28 @@ class TodayLecturingSchedulesTest extends TestCase
     /** @test */
     public function user_can_see_lecturing_today_card()
     {
-        $user = factory(User::class)->create(['name' => 'Nama Member', 'email' => 'email@mail.com']);
-        $this->loginAsUser();
         factory(LecturingSchedule::class)->create();
         $this->visit('/');
-        Livewire::test(TodayLecturerCard::class)
-            ->assertSeeHtml('<h3 class="card-title flex-grow-1">' .
-                __('lecturing_schedule.lecturing_schedule') . ' ' .
-                __('time.today') . '</h3>');
-    }
-
-    /** @test */
-    public function user_can_see_carousel_lecturing_today_card()
-    {
-        $user = factory(User::class)->create(['name' => 'Nama Member', 'email' => 'email@mail.com']);
-        $this->loginAsUser();
-        factory(LecturingSchedule::class, 3)->create();
-        $this->visit('/');
-        Livewire::test(TodayLecturerCard::class)
-            ->assertCount('lecturingSchedules', 3)
-            ->assertSeeHtml('<h3 class="card-title flex-grow-1">' .
-                __('lecturing_schedule.lecturing_schedule') . ' ' .
-                __('time.today') . '</h3>');
+        Livewire::test(TodayLecturingSchedules::class)
+            ->assertSee(__('lecturing_schedule.public_schedule') . ' ' . __('time.today'));
     }
 
     /** @test */
     public function user_can_see_firday_lecturing_today_card()
     {
-        Carbon::setTestNow("15-09-2023");
-        $user = factory(User::class)->create(
+        Carbon::setTestNow("16-09-2023");
+        factory(User::class)->create(
             ['name' => 'Nama Member', 'email' => 'email@mail.com']
         );
         $this->loginAsUser();
         factory(LecturingSchedule::class)->create([
             'audience_code' => LecturingSchedule::AUDIENCE_FRIDAY
         ]);
+        factory(LecturingSchedule::class)->create();
         $this->visit('/');
-        Livewire::test(TodayLecturerCard::class)
-            ->assertCount('lecturingSchedules', 1)
-            ->assertSeeHtml('<h3 class="card-title flex-grow-1">' .
-                __('lecturing_schedule.friday_lecturing_schedule') . ' ' .
-                __('time.today') . '</h3>');
+        Livewire::test(TodayLecturingSchedules::class)
+            ->assertCount('lecturingSchedules', 2)
+            ->assertSee(__('lecturing_schedule.friday_lecturer_name'))
+            ->assertSee(__('lecturing_schedule.audience_' . LecturingSchedule::AUDIENCE_FRIDAY));
     }
 }
