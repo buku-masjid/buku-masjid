090a39926f157fdf0c8deeb4dbba14ff8471b45e
Nafies Luthfi <nafiesl@gmail.com>
2023-09-29 11:50:45 +0800

    feat: Remove duplicated category

--- database/seeders/DefaultCatergoryTableSeeder.php
+++ database/seeders/DefaultCatergoryTableSeeder.php
@@ -16,7 +16,6 @@ class DefaultCatergoryTableSeeder extends Seeder
         Category::create(['name' => 'Gaji Karyawan', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_INTERNAL, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Insentif Hari Jumat', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Insentif Kajian Subuh', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
-        Category::create(['name' => 'Insentif Kajian Sabtu Sore', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Konsumsi Kajian Subuh', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Tagihan Air', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Tagihan Listrik', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
