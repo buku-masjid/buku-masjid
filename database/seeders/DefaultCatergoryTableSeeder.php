75f4b040aa914f71a88fe5e32e481a74dda43889
Nafies Luthfi <nafiesl@gmail.com>
2023-09-29 21:13:18 +0800

    feat: Update lecturing transction category names

--- database/seeders/DefaultCatergoryTableSeeder.php
+++ database/seeders/DefaultCatergoryTableSeeder.php
@@ -10,13 +10,13 @@ class DefaultCatergoryTableSeeder extends Seeder
     public function run(): void
     {
         Category::create(['name' => 'Kotak Infaq Hari Jumat', 'color' => '#00AABB', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
-        Category::create(['name' => 'Kotak Infaq Kajian Subuh', 'color' => '#00AABB', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
+        Category::create(['name' => 'Kotak Infaq Kajian', 'color' => '#00AABB', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Kotak Infaq Harian', 'color' => '#00AABB', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Pemasukan Infaq Lain-lain', 'color' => '#00AABB', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Gaji Karyawan', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_INTERNAL, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Insentif Hari Jumat', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
-        Category::create(['name' => 'Insentif Kajian Subuh', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
-        Category::create(['name' => 'Konsumsi Kajian Subuh', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
+        Category::create(['name' => 'Insentif Kajian', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
+        Category::create(['name' => 'Konsumsi Kajian', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Tagihan Air', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Tagihan Listrik', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
         Category::create(['name' => 'Tagihan Internet', 'color' => '#F16867', 'report_visibility_code' => Category::REPORT_VISIBILITY_PUBLIC, 'creator_id' => 1, 'book_id' => 1]);
