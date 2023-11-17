d6f9c1f82c570f09ccd8bcc0343930888906678b
Nafies Luthfi <nafiesl@gmail.com>
2023-11-17 23:59:10 +0800

    feat: Added book report setting information

--- resources/views/layouts/public_reports.blade.php
+++ resources/views/layouts/public_reports.blade.php
@@ -6,8 +6,16 @@
 
 @section('content')
 
-<div class=" text-center mt-0 mb-2">
+<div class="page-header mt-0">
     <h1 class="page-title">{{ __('book.book') }}: {{ auth()->activeBook()->name }}</h1>
+    <div class="page-options d-flex">
+        <span class="badge text-uppercase badge-primary mr-1" title="{{ __('report.periode') }}: {{ __('report.'.auth()->activeBook()->report_periode_code) }}">
+            {{ __('report.'.auth()->activeBook()->report_periode_code) }}
+        </span>
+        <span class="badge text-uppercase badge-warning text-dark" title="{{ __('report.start_week_day') }}: {{ __('time.days.'.auth()->activeBook()->start_week_day_code) }}">
+            {{ __('time.days.'.auth()->activeBook()->start_week_day_code) }}
+        </span>
+    </div>
 </div>
 @include('layouts._public_report_nav')
 <hr class="mt-2 mb-4">
