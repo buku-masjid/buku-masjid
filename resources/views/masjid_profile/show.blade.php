30415e59b7d860b6e42abecb5d88a778f31a0378
Frendi Triarista <frendi@thriveagency.id>
2024-02-10 19:24:22 +0700

    Show Masjid Image Profile

--- resources/views/masjid_profile/show.blade.php
+++ resources/views/masjid_profile/show.blade.php
@@ -11,6 +11,13 @@
         <div class="card">
             <table class="table table-sm card-table">
                 <tbody>
+                    <tr>
+                        <td colspan="2" class="text-center">
+                        @if (Setting::get('masjid_image'))
+                            <img class="img-fluid my-4" src="{{ asset(Setting::get('masjid_image'))}}" alt="{{ Setting::get('masjid_name') ?? 'buku masjid'}}">
+                        @endif
+                        </td>
+                    </tr>
                     <tr><td class="col-4">{{ __('masjid_profile.name') }}</td><td>{{ Setting::get('masjid_name', config('masjid.name')) }}</td></tr>
                     <tr><td>{{ __('masjid_profile.address') }}</td><td>{{ Setting::get('masjid_address') }}</td></tr>
                     <tr><td>{{ __('masjid_profile.google_maps_link') }}</td><td>{{ Setting::get('masjid_google_maps_link') }}</td></tr>
