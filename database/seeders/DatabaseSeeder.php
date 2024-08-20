380f0f805bb2cc3b78b4c8c7256f502f7ada9a8f
Nafies Luthfi <nafiesl@gmail.com>
2024-08-20 20:42:41 +0800

    feat: Set finance user as default book manager on DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,8 +8,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(DefaultBookTableSeeder::class);
         $this->call(DefaultUserTableSeeder::class);
+        $this->call(DefaultBookTableSeeder::class);
         $this->call(DefaultCatergoryTableSeeder::class);
     }
 }
--- database/seeders/DefaultBookTableSeeder.php
+++ database/seeders/DefaultBookTableSeeder.php
@@ -3,17 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DefaultBookTableSeeder extends Seeder
 {
     public function run(): void
     {
+        $financeUser = User::where('role_id', User::ROLE_FINANCE)->where('is_active', 1)->first();
+
         Book::create([
             'name' => 'Kegiatan Rutin',
             'description' => 'Buku catatan keuangan kegiatan rutin',
             'creator_id' => null,
             'report_visibility_code' => 'public',
+            'manager_id' => optional($financeUser)->id,
         ]);
     }
 }
