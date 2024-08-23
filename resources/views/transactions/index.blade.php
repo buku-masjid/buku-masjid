54304ede036b8e8c447459a1c5c05ad36f4a0254
Nafies Luthfi <nafiesl@gmail.com>
2024-08-23 10:45:49 +0800

    fix: Table responsive

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -25,132 +25,134 @@
                 @include('transactions.partials.index_filters')
             </div>
             @desktop
-            <table class="table table-sm table-responsive-sm table-hover table-bordered mb-0">
-                <thead>
-                    <tr>
-                        <th class="text-center">{{ __('app.table_no') }}</th>
-                        <th class="text-center">{{ __('app.date') }}</th>
-                        <th>{{ __('transaction.description') }}</th>
-                        <th class="text-right">{{ __('transaction.amount') }}</th>
-                        <th class="text-center">{{ __('app.action') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($transactions as $key => $transaction)
-                    @php
-                        $groups = $transactions->where('date_only', $transaction->date_only);
-                        $firstGroup = $groups->first();
-                        $groupCount = $groups->count();
-                    @endphp
-                    <tr>
-                        <td class="text-center">{{ 1 + $key }}</td>
-                        @if ($firstGroup->id == $transaction->id)
-                            <td class="text-center text-middle" rowspan="{{ $groupCount }}">
-                                {{ $transaction->day_name }},
-                                {{ link_to_route('transactions.index', $transaction->date_only.'-'.$transaction->month_name, [
-                                    'date' => $transaction->date_only,
-                                    'month' => $month,
-                                    'year' => $year,
-                                    'category_id' => request('category_id'),
-                                ]) }}
-                            </td>
-                        @endif
-                        <td>
-                            <span class="float-right">
-                                <span class="badge {{ $transaction->bankAccount->exists ? 'bg-purple' : 'bg-gray'}}">
-                                    {{ $transaction->bankAccount->name }}
+            <div class="table-responsive-sm">
+                <table class="table table-sm table-hover table-bordered mb-0">
+                    <thead>
+                        <tr>
+                            <th class="text-center">{{ __('app.table_no') }}</th>
+                            <th class="text-center">{{ __('app.date') }}</th>
+                            <th>{{ __('transaction.description') }}</th>
+                            <th class="text-right">{{ __('transaction.amount') }}</th>
+                            <th class="text-center">{{ __('app.action') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($transactions as $key => $transaction)
+                        @php
+                            $groups = $transactions->where('date_only', $transaction->date_only);
+                            $firstGroup = $groups->first();
+                            $groupCount = $groups->count();
+                        @endphp
+                        <tr>
+                            <td class="text-center">{{ 1 + $key }}</td>
+                            @if ($firstGroup->id == $transaction->id)
+                                <td class="text-center text-middle" rowspan="{{ $groupCount }}">
+                                    {{ $transaction->day_name }},
+                                    {{ link_to_route('transactions.index', $transaction->date_only.'-'.$transaction->month_name, [
+                                        'date' => $transaction->date_only,
+                                        'month' => $month,
+                                        'year' => $year,
+                                        'category_id' => request('category_id'),
+                                    ]) }}
+                                </td>
+                            @endif
+                            <td>
+                                <span class="float-right">
+                                    <span class="badge {{ $transaction->bankAccount->exists ? 'bg-purple' : 'bg-gray'}}">
+                                        {{ $transaction->bankAccount->name }}
+                                    </span>
+                                    @if ($transaction->category)
+                                        @php
+                                            $categoryRoute = route('categories.show', [
+                                                $transaction->category_id,
+                                                'start_date' => $startDate,
+                                                'end_date' => $year.'-'.$month.'-'.date('t'),
+                                            ]);
+                                        @endphp
+                                        <a href="{{ $categoryRoute }}">{!! optional($transaction->category)->name_label !!}</a>
+                                    @endif
                                 </span>
-                                @if ($transaction->category)
-                                    @php
-                                        $categoryRoute = route('categories.show', [
-                                            $transaction->category_id,
-                                            'start_date' => $startDate,
-                                            'end_date' => $year.'-'.$month.'-'.date('t'),
-                                        ]);
-                                    @endphp
-                                    <a href="{{ $categoryRoute }}">{!! optional($transaction->category)->name_label !!}</a>
-                                @endif
-                            </span>
-                            {{ $transaction->description }}
-                        </td>
-                        <td class="text-right">{{ $transaction->amount_string }}</td>
-                        <td class="text-center">
-                            @can('update', $transaction)
-                                @can('manage-transactions', auth()->activeBook())
-                                    {!! link_to_route(
-                                        'transactions.index',
-                                        __('app.edit'),
-                                        ['action' => 'edit', 'id' => $transaction->id] + request(['month', 'year', 'query', 'category_id']),
-                                        ['id' => 'edit-transaction-'.$transaction->id]
-                                    ) !!}
+                                {{ $transaction->description }}
+                            </td>
+                            <td class="text-right">{{ $transaction->amount_string }}</td>
+                            <td class="text-center">
+                                @can('update', $transaction)
+                                    @can('manage-transactions', auth()->activeBook())
+                                        {!! link_to_route(
+                                            'transactions.index',
+                                            __('app.edit'),
+                                            ['action' => 'edit', 'id' => $transaction->id] + request(['month', 'year', 'query', 'category_id']),
+                                            ['id' => 'edit-transaction-'.$transaction->id]
+                                        ) !!}
+                                    @endcan
                                 @endcan
-                            @endcan
-                        </td>
-                    </tr>
-                    @empty
-                    <tr><td colspan="5">{{ __('transaction.not_found') }}</td></tr>
-                    @endforelse
-                </tbody>
-                @if (request('category_id') || request('book_id'))
-                <tfoot>
-                    <tr><td colspan="5" class="text-right">&nbsp;</td></tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.income_total') }}</td>
-                        <td class="text-right">{{ format_number($incomeTotal) }}</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.spending_total') }}</td>
-                        <td class="text-right">{{ format_number($spendingTotal) }}</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.difference') }}</td>
-                        <td class="text-right">{{ format_number($incomeTotal - $spendingTotal) }}</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                </tfoot>
-                @else
-                <tfoot>
-                    <tr><td colspan="5" class="text-right">&nbsp;</td></tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.start_balance') }}</td>
-                        <td class="text-right">
-                            @php
-                                $balance = 0;
-                            @endphp
-                            @if ($transactions->first())
-                                {{ format_number($balance = auth()->activeBook()->getBalance(Carbon\Carbon::parse($transactions->first()->date)->subDay()->format('Y-m-d'))) }}
-                            @else
-                                0
-                            @endif
-                        </td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.income_total') }}</td>
-                        <td class="text-right">{{ format_number($incomeTotal) }}</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.spending_total') }}</td>
-                        <td class="text-right">{{ format_number($spendingTotal) }}</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr class="strong">
-                        <td colspan="3" class="text-right">{{ __('transaction.end_balance') }}</td>
-                        <td class="text-right">
-                            @if ($transactions->first())
-                                {{ format_number($balance + $incomeTotal - $spendingTotal) }}
-                            @else
-                                0
-                            @endif
-                        </td>
-                        <td>&nbsp;</td>
-                    </tr>
-                </tfoot>
-                @endif
-            </table>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr><td colspan="5">{{ __('transaction.not_found') }}</td></tr>
+                        @endforelse
+                    </tbody>
+                    @if (request('category_id') || request('book_id'))
+                    <tfoot>
+                        <tr><td colspan="5" class="text-right">&nbsp;</td></tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.income_total') }}</td>
+                            <td class="text-right">{{ format_number($incomeTotal) }}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.spending_total') }}</td>
+                            <td class="text-right">{{ format_number($spendingTotal) }}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.difference') }}</td>
+                            <td class="text-right">{{ format_number($incomeTotal - $spendingTotal) }}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tfoot>
+                    @else
+                    <tfoot>
+                        <tr><td colspan="5" class="text-right">&nbsp;</td></tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.start_balance') }}</td>
+                            <td class="text-right">
+                                @php
+                                    $balance = 0;
+                                @endphp
+                                @if ($transactions->first())
+                                    {{ format_number($balance = auth()->activeBook()->getBalance(Carbon\Carbon::parse($transactions->first()->date)->subDay()->format('Y-m-d'))) }}
+                                @else
+                                    0
+                                @endif
+                            </td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.income_total') }}</td>
+                            <td class="text-right">{{ format_number($incomeTotal) }}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.spending_total') }}</td>
+                            <td class="text-right">{{ format_number($spendingTotal) }}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr class="strong">
+                            <td colspan="3" class="text-right">{{ __('transaction.end_balance') }}</td>
+                            <td class="text-right">
+                                @if ($transactions->first())
+                                    {{ format_number($balance + $incomeTotal - $spendingTotal) }}
+                                @else
+                                    0
+                                @endif
+                            </td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tfoot>
+                    @endif
+                </table>
+            </div>
             @elsedesktop
             <div class="card-body">
                 @foreach ($transactions->groupBy('date') as $groupedTransactions)
