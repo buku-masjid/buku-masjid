d92a9da3e53f1e7c874a1de5072aa6f8991b3458
Nafies Luthfi <nafiesl@gmail.com>
2024-09-01 22:35:14 +0800

    feat: Update partner factory

--- database/factories/PartnerFactory.php
+++ database/factories/PartnerFactory.php
@@ -3,14 +3,12 @@
 use App\Models\Partner;
 use App\User;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 $factory->define(Partner::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'type_code' => 'partner',
-        'phone' => Str::random(10),
-        'name' => $faker->name,
+        'phone' => $faker->phoneNumber,
         'is_active' => Partner::STATUS_ACTIVE,
         'creator_id' => function () {
             return factory(User::class)->create()->id;
