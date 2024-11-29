50e4500e4d688994d791d21f7ae853b802f6c885
Nafies Luthfi <nafiesl@gmail.com>
2024-11-29 22:39:10 +0800

    feat: Add next and prev year and month navigations

--- resources/views/donors/_partner_statistics.blade.php
+++ resources/views/donors/_partner_statistics.blade.php
@@ -7,16 +7,23 @@
     </div>
     <div class="col-md-6 mt-3 mt-sm-0">
         {{ Form::open(['method' => 'get', 'class' => 'form-inline justify-content-center mt-3 mx-3']) }}
-        {{ Form::select('book_id', ['' => '-- '.__('book.all').' --'] + $availableBooks, optional($selectedBook)->id, ['class' => 'form-control mr-1']) }}
-        {{ Form::select('year', get_years(), $selectedYear, ['class' => 'form-control mr-1']) }}
-        @if ($selectedYear !== '0000')
-            {{ Form::select('month', ['00' => '-- '.__('time.month').' --'] + get_months(), $selectedMonth, ['class' => 'form-control mr-1']) }}
-        @endif
-        <div class="form-group mt-4 mt-sm-0 pt-0 pt-sm-2">
-            {{ Form::submit(__('report.view_report'), ['class' => 'btn btn-info mr-2']) }}
-            {{ link_to_route('donors.index', __('report.this_month'), [], ['class' => 'btn btn-secondary mr-2']) }}
+        {{ Form::select('book_id', ['' => '-- '.__('book.all').' --'] + $availableBooks, optional($selectedBook)->id, ['class' => 'form-control mr-1', 'onchange' => 'submit()']) }}
+        {{ Form::select('year', get_years(), $selectedYear, ['class' => 'form-control mr-1', 'onchange' => 'submit()']) }}
+        {{ Form::select('month', ['00' => '-- '.__('time.month').' --'] + get_months(), $selectedMonth, ['class' => 'form-control mr-1', 'onchange' => 'submit()']) }}
+        <div class="form-group text-center">
+            @if ($selectedMonth == '00')
+                {{ link_to_route('donors.index', __('report.prev_year'), ['year' => $selectedYear - 1, 'month' => '00'], ['class' => 'btn btn-gray mt-2 mr-1']) }}
+                {{ link_to_route('donors.index', __('report.this_year'), ['year' => today()->format('Y'), 'month' => '00'], ['class' => 'btn btn-gray mt-2 mr-1']) }}
+                {{ link_to_route('donors.index', __('report.next_year'), ['year' => $selectedYear + 1, 'month' => '00'], ['class' => 'btn btn-gray mt-2 mr-1']) }}
+                {{ link_to_route('donors.index', __('report.this_month'), [], ['class' => 'btn btn-secondary mt-2 mr-1']) }}
+            @else
+                @livewire('prev-month-button', ['routeName' => 'donors.index', 'buttonClass' => 'btn btn-secondary mt-2 mr-1'])
+                {{ link_to_route('donors.index', __('report.this_month'), [], ['class' => 'btn btn-secondary mt-2 mr-1']) }}
+                @livewire('next-month-button', ['routeName' => 'donors.index', 'buttonClass' => 'btn btn-secondary mt-2 mr-1'])
+                {{ link_to_route('donors.index', __('report.this_year'), ['year' => today()->format('Y'), 'month' => '00'], ['class' => 'btn btn-gray mt-2 mr-1']) }}
+            @endif
             @can('create', new App\Transaction)
-                {{ link_to_route('donor_transactions.create', __('donor.add_donation'), [], ['class' => 'btn btn-success']) }}
+                {{ link_to_route('donor_transactions.create', __('donor.add_donation'), [], ['class' => 'btn btn-success mt-2']) }}
             @endcan
         </div>
         {{ Form::close() }}
