8fd03df1f41c3589d7492ab44100c52b8edb24e5
Nafies Luthfi <nafiesl@gmail.com>
2025-01-16 17:49:41 +0800

    feat: Show available images each transaction

--- resources/views/public_reports/finance/_public_content_detailed.blade.php
+++ resources/views/public_reports/finance/_public_content_detailed.blade.php
@@ -49,6 +49,26 @@
                         <tr class="{{ $transaction->is_strong ? 'strong' : '' }}">
                             <td class="text-center">{{ $transaction->date }}</td>
                             <td {{ $transaction->is_strong ? 'style=text-decoration:underline' : '' }}>
+                                @if ($transaction->files->count())
+                                    <div class="dropdown float-right">
+                                        <a class="badge badge-light text-dark" data-toggle="dropdown" aria-expanded="false" style="cursor:pointer">
+                                            {{ $transaction->files->count() }} <i class="fe fe-image"></i>
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            @foreach ($transaction->files as $key => $file)
+                                                <div class="dropdown-item">
+                                                    <a href="{{ asset('storage/'.$file->file_path) }}">
+                                                        @if ($file->title)
+                                                            <div>{{ $file->title }}</div>
+                                                        @else
+                                                            <div>{{ __('transaction.files') }} {{ 1 + $key }}</div>
+                                                        @endif
+                                                    </a>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
                                 {!! $transaction->date_alert !!} {{ $transaction->description }}
                             </td>
                             <td class="text-right text-nowrap">{{ $transaction->in_out ? format_number($transaction->amount) : '' }}</td>
