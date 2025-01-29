4ecc9439b26b7718a6ec1274f43835cc3a5e4014
Nafies Luthfi <nafiesl@gmail.com>
2025-01-29 10:25:04 +0800

    feat: Remove unused elements

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -19,44 +19,8 @@
     <div class="container-md home-bottom">
         <div>
             <div class="row p-3 align-items-end">
-                <!--<div class="col-lg-9">
-                    <div class="fs-4 pt-3 pb-3 d-flex align-items-center">
-                        <span class="fs-2 fw-bold pe-2">Laporan Pekan Ini</span>
-                        <span class="badge bg-cyan-lt">Kegiatan Rutin</span>
-                    </div>
-                    <div class="row align-items-end">
-                        <div class="col-lg ps-sm-0">
-                            <div class="card fw-bold p-3 mb-2 shadow-lg">
-                                Pemasukan<br>
-                                <span class="date">7 Oktober 2024</span>
-                                <h1 class="pt-4 bm-txt-primary fw-bolder">Rp 570.000</h1>
-                            </div>
-                        </div>
-                        <div class="col-lg ps-sm-0">
-                            <div class="card fw-bold p-3 mb-2 shadow-lg">
-                                Pengeluaran<br>
-                                <span class="date">7 Oktober 2024</span>
-                                <h1 class="pt-4 bm-txt-out fw-bolder">Rp 570.000</h1>
-                            </div>
-                        </div>
-                        <div class="col-lg ps-sm-0">
-                            <div class="card fs-3 fw-bold p-3 mb-2 position-relative shadow-lg">
-                                <button type="button" class="fs-6 btn btn-sm bm-btn btn-outline-cyan position-absolute end-0 me-3 px-2 py-1">
-                                Lebih Detil
-                                </button>
-                                Saldo Terakhir<br>
-                                <span class="date">7 Oktober 2024</span>
-                                <h1 class="pt-4 bm-txt-netral fw-bolder">Rp 570.000</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="d-sm-none">
-                        <button type="button" class="btn bm-btn btn-sm btn-ghost-cyan mt-2">
-                            &nbsp;Lihat Semua Laporan <i class="ti">&#xea1c;</i>&nbsp;
-                        </button>
-                    </a>
-                </div>-->
                 @livewire('public-home.weekly-financial-summary')
+                {{--
                 <div class="col-lg-3 ps-sm-0">
                     <div class="fs-2 fw-bold pb-3 pt-sm-3">
                         <br class="d-sm-none">
@@ -77,6 +41,7 @@
                         <h1 class="pt-4 fw-bolder">7199793588</h1>
                     </div>
                 </div>
+                --}}
             </div>
         </div>
     </div>
