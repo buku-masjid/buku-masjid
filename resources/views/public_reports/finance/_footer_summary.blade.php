ad546682d86bfaa04829190e939fb8f6faa4ac8a
Nafies Luthfi <nafiesl@gmail.com>
2025-01-29 08:33:47 +0800

    feat Add starting and ending balance

--- resources/views/public_reports/finance/_footer_summary.blade.php
+++ resources/views/public_reports/finance/_footer_summary.blade.php
@@ -1,5 +1,11 @@
+<div class="fs-3 fw-bold text-center text-sm-end border-top pt-3 px-lg-4">
+    {{ __('transaction.start_balance') }}
+    <h1 class="bm-txt-dark fw-bolder">{{ format_number($lastMonthBalance) }}</h1>
+</div>
 <div class="row fs-3 py-3 px-lg-4 py-lg-3">
-    <div class="col text-center text-sm-start fs-2 fw-bold py-3">{{ __('report.balance_per_date', ['date' => $currentMonthEndDate->isoFormat('D MMMM Y')]) }}</div>
+    <div class="col text-center text-sm-start fs-2 fw-bold py-3">
+        {{ __('report.balance_per_date', ['date' => $currentMonthEndDate->isoFormat('D MMMM Y')]) }}
+    </div>
     <div class="col-sm-auto text-center pb-2 pb-sm-0 text-sm-end">
         {{ __('transaction.income') }}
         <h2 class="bm-txt-primary ">{{ format_number($currentMonthIncome) }}</h2>
@@ -8,8 +14,12 @@
         {{ __('transaction.spending') }}
         <h2 class="bm-txt-out ">{{ format_number($currentMonthSpending) }}</h2>
     </div>
+    <div class="col-sm-auto text-center pt-3 pt-sm-0  text-sm-end">
+        {{ __('transaction.balance') }}
+        <h2 class="bm-txt-dark ">{{ format_number($currentMonthBalance) }}</h2>
+    </div>
 </div>
 <div class="fs-3 fw-bold text-center text-sm-end border-top pt-3 px-lg-4">
     {{ __('transaction.end_balance') }}
-    <h1 class="bm-txt-netral fw-bolder">{{ format_number($currentMonthBalance) }}</h1>
+    <h1 class="bm-txt-netral fw-bolder">{{ format_number($lastMonthBalance + $currentMonthBalance) }}</h1>
 </div>
--- resources/views/public_reports/finance/_header_summary.blade.php
+++ resources/views/public_reports/finance/_header_summary.blade.php
@@ -1,6 +1,14 @@
 <div class="row pt-2 pt-sm-3 gap-2">
     <div class="col pe-0">
         <div class="card fw-bold p-3 mb-2 shadow">
+            {{ __('transaction.start_balance') }}<br>
+            <span class="date">{{ $startDate->isoFormat('dddd, D MMMM Y') }}</span>
+            <h1 class="pt-4 bm-txt-dark fw-bolder">{{ format_number($lastMonthBalance) }}</h1>
+            <i class="ti fe-normal bm-txt-dark bm-bg-primary-soft position-absolute top-50 end-0 translate-middle p-2">&#xeb75;</i>
+        </div>
+    </div>
+    <div class="col px-0">
+        <div class="card fw-bold p-3 mb-2 shadow">
             {{ __('transaction.income') }}<br>
             <span class="date">{{ $currentMonthEndDate->isoFormat('dddd, D MMMM Y') }}</span>
             <h1 class="pt-4 bm-txt-primary fw-bolder">{{ format_number($currentMonthIncome) }}</h1>
@@ -20,7 +28,7 @@
             {{ __('transaction.end_balance') }}<br>
             <span class="date">{{ $currentMonthEndDate->isoFormat('dddd, D MMMM Y') }}</span>
             <h1 class="pt-4 bm-txt-netral fw-bolder">
-                {{ format_number($currentMonthBalance) }}
+                {{ format_number($lastMonthBalance + $currentMonthBalance) }}
             </h1>
             <i class="ti fe-normal bm-txt-netral bm-bg-netral-soft position-absolute top-50 end-0 translate-middle p-2">&#xeb75;</i>
         </div>
