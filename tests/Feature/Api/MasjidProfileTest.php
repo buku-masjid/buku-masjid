3bc9cf7049a66250d94e96738b1ecd6327240b80
Nafies Luthfi <nafiesl@gmail.com>
2025-01-31 18:44:11 +0800

    test: Add test for masjid logo and photo upload

--- tests/Feature/Api/MasjidProfileTest.php
+++ tests/Feature/Api/MasjidProfileTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Api;
 
 use Facades\App\Helpers\Setting;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -31,6 +33,70 @@ class MasjidProfileTest extends TestCase
             'masjid_address' => $masjidAddress,
             'google_maps_link' => $masjidGoogleMapsLink,
             'logo_image_url' => Storage::url($logoImageUrl),
+        ]);
+    }
+
+    /** @test */
+    public function update_masjid_logo_with_csrf_token()
+    {
+        $this->loginAsUser();
+        $this->dontSeeInDatabase('settings', ['key' => 'masjid_logo_path']);
+
+        $this->get(route('home'));
+        $this->seeStatusCode(200);
+
+        $csrfToken = csrf_token();
+        Storage::fake(config('filesystem.default'));
+        $image = UploadedFile::fake()->image('logo.jpg');
+        $base64Image = 'data:image/png;base64,'.base64_encode(file_get_contents($image->getPathname()));
+
+        $this->post(route('api.masjid_profile.upload_logo'), [
+            '_token' => $csrfToken,
+            'image' => $base64Image,
+        ]);
+
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('settings', [
+            'key' => 'masjid_logo_path',
+        ]);
+
+        $settingRecord = DB::table('settings')->where('key', 'masjid_logo_path')->first();
+        Storage::assertExists($settingRecord->value);
+        $this->seeJson([
+            'message' => __('masjid_profile.logo_uploaded'),
+            'image' => Storage::url($settingRecord->value),
+        ]);
+    }
+
+    /** @test */
+    public function update_masjid_photo_with_csrf_token()
+    {
+        $this->loginAsUser();
+        $this->dontSeeInDatabase('settings', ['key' => 'masjid_photo_path']);
+
+        $this->get(route('home'));
+        $this->seeStatusCode(200);
+
+        $csrfToken = csrf_token();
+        Storage::fake(config('filesystem.default'));
+        $image = UploadedFile::fake()->image('photo.jpg');
+        $base64Image = 'data:image/png;base64,'.base64_encode(file_get_contents($image->getPathname()));
+
+        $this->post(route('api.masjid_profile.upload_photo'), [
+            '_token' => $csrfToken,
+            'image' => $base64Image,
+        ]);
+
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('settings', [
+            'key' => 'masjid_photo_path',
+        ]);
+
+        $settingRecord = DB::table('settings')->where('key', 'masjid_photo_path')->first();
+        Storage::assertExists($settingRecord->value);
+        $this->seeJson([
+            'message' => __('masjid_profile.photo_uploaded'),
+            'image' => Storage::url($settingRecord->value),
         ]);
     }
 }
