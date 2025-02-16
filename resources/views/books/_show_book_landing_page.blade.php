98518700ab83662f128f5c59aa128d2f4d510d73
Nafies Luthfi <nafiesl@gmail.com>
2025-02-16 23:19:43 +0800

    feat: Restructure book landing page detail

--- resources/views/books/_show_book_landing_page.blade.php
+++ resources/views/books/_show_book_landing_page.blade.php
@@ -1,27 +1,26 @@
-<div class="row">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">
-                {{ __('book.landing_page') }}
-                <div class="card-options">
-                    @can('update', $book)
-                        {{ link_to_route('books.edit', __('app.edit'), [$book, 'tab' => 'landing_page'], ['class' => 'btn btn-sm btn-warning text-dark mr-2', 'id' => 'edit_landing_page-book-'.$book->id]) }}
-                    @endcan
-                </div>
-            </div>
-            <div class="card-body">
-                {{ __('book.due_date') }}: {{ Setting::for($book)->get('due_date') }}
-            </div>
-            <div class="card-body">
-                @if (Setting::for($book)->get('landing_page_content'))
-                    {!! Purify::clean(Setting::for($book)->get('landing_page_content')) !!}
-                @else
-                    {{ __('book.landing_page_content') }}
-                @endif
-            </div>
+<div class="card">
+    <div class="card-header">
+        {{ __('book.landing_page') }}
+        <div class="card-options">
+            @can('update', $book)
+                {{ link_to_route('books.edit', __('app.edit'), [$book, 'tab' => 'landing_page'], ['class' => 'btn btn-sm btn-warning text-dark mr-2', 'id' => 'edit_landing_page-book-'.$book->id]) }}
+            @endcan
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="card-body">
+        {{ __('book.due_date') }}: {{ Setting::for($book)->get('due_date') }}
+    </div>
+    <div class="card-body">
+        @if (Setting::for($book)->get('landing_page_content'))
+            {!! Purify::clean(Setting::for($book)->get('landing_page_content')) !!}
+        @else
+            {{ __('book.landing_page_content') }}
+        @endif
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-8">
         <div class="card">
             <div class="card-header">
                 {{ __('book.poster_image') }}
@@ -34,6 +33,8 @@
                 @endif
             </div>
         </div>
+    </div>
+    <div class="col-md-4">
         <div class="card">
             <div class="card-header">
                 {{ __('book.thumbnail_image') }}
