284f02bd6ec71649f2a2838c53a41303699e8010
Nafies Luthfi <nafiesl@gmail.com>
2025-02-25 18:20:18 +0800

    fix: Financial summary for public page

--- resources/views/livewire/books/financial_summary.blade.php
+++ resources/views/livewire/books/financial_summary.blade.php
@@ -12,9 +12,9 @@
             <div class="card fw-bold p-3 mb-2 shadow-lg">
                 {{ $currentPeriodeBudgetLabel }}
                 <h1 class="pt-4 bm-txt-netral fw-bolder">
-                    <span id="current_budget">{{ config('money.currency_code') }}{{ format_number($currentBudget) }}</span>
+                    <span id="current_budget">{{ config('money.currency_code') }} {{ format_number($currentBudget) }}</span>
                 </h1>
-                <span class="date">{{ config('money.currency_code') }}{{ format_number($currentBudget) }}</span>
+                <span class="date">{{ config('money.currency_code') }} {{ format_number($currentBudget) }}</span>
             </div>
         </div>
     @endif
@@ -22,9 +22,9 @@
         <div class="card fw-bold p-3 mb-2 shadow-lg">
             {{ __('report.current_periode_income_total') }}
             <h1 class="pt-4 bm-txt-primary fw-bolder">
-                <span id="current_income_total">{{ config('money.currency_code')}}{{ format_number($currentIncomeTotal) }}</span>
+                <span id="current_income_total">{{ config('money.currency_code')}} {{ format_number($currentIncomeTotal) }}</span>
             </h1>
-            <span class="date">{{ config('money.currency_code') }}{{ format_number($currentIncomeTotal) }}</span>
+            <span class="date">{{ config('money.currency_code') }} {{ format_number($currentIncomeTotal) }}</span>
         </div>
     </div>
     @if ($currentBudget)
@@ -32,9 +32,9 @@
             <div class="card fw-bold p-3 mb-2 shadow-lg">
                 {{ $currentBudgetRemainingLabel }}
                 <h1 class="pt-4 bm-txt-out fw-bolder">
-                    <span id="current_diff">{{ config('money.currency_code') }}{{ format_number($budgetDifference) }}</span>
+                    <span id="current_diff">{{ config('money.currency_code') }} {{ format_number($budgetDifference) }}</span>
                 </h1>
-                <span class="date">{{ config('money.currency_code') }}{{ format_number($budgetDifference) }}</span>
+                <span class="date">{{ config('money.currency_code') }} {{ format_number($budgetDifference) }}</span>
             </div>
         </div>
     @else
@@ -42,37 +42,35 @@
             <div class="card fw-bold p-3 mb-2 shadow-lg">
                 {{ __('report.current_periode_spending_total') }}
                 <h1 class="pt-4 bm-txt-out fw-bolder">
-                    <span id="current_income_total">{{ config('money.currency_code') }}{{ format_number($currentSpendingTotal ? -$currentSpendingTotal : 0) }}</span>
+                    <span id="current_income_total">{{ config('money.currency_code') }} {{ format_number($currentSpendingTotal ? -$currentSpendingTotal : 0) }}</span>
                 </h1>
-                <span class="date">{{ config('money.currency_code') }}{{ format_number($currentSpendingTotal ? -$currentSpendingTotal : 0) }}</span>
+                <span class="date">{{ config('money.currency_code') }} {{ format_number($currentSpendingTotal ? -$currentSpendingTotal : 0) }}</span>
             </div>
         </div>
         <div class="col-lg ps-sm-0">
             <div class="card fw-bold p-3 mb-2 shadow-lg">
                 {{ __('report.today_balance', ['date' => $today->isoFormat('dddd, D MMM Y')]) }}
                 <h1 class="pt-4 bm-txt-primary fw-bolder">
-                    <span id="current_income_total">{{ config('money.currency_code') }}{{ format_number($currentBalance) }}</span>
+                    <span id="current_income_total">{{ config('money.currency_code') }} {{ format_number($currentBalance) }}</span>
                 </h1>
-                <span class="date">{{ config('money.currency_code') }}{{ format_number($currentBalance) }}</span>
+                <span class="date">{{ config('money.currency_code') }} {{ format_number($currentBalance) }}</span>
             </div>
         </div>
     @endif
 <div>
 
 <script>
-    /*function shortenNominalConcise(amount) {
-        const formatter = new Intl.NumberFormat('id-ID', { 
-            notation: 'compact', 
+    function shortenNominalConcise(amount) {
+        const formatter = new Intl.NumberFormat('id-ID', {
+            notation: 'compact',
             maximumFractionDigits: 2 // decimal places.
         });
-        return `Rp${formatter.format(amount)}`;
+        return `Rp ${formatter.format(amount)}`;
     }
 
     const currentBudget = document.getElementById('current_budget');
     currentBudget.textContent = shortenNominalConcise(parseInt({{$currentBudget}}));
 
     const currentIncomeTotal = document.getElementById('current_income_total');
-    currentIncomeTotal.textContent = shortenNominalConcise(parseInt({{$currentIncomeTotal}})); */
-
-    
+    currentIncomeTotal.textContent = shortenNominalConcise(parseInt({{$currentIncomeTotal}}));
 </script>
