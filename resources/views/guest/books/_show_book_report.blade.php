f61dc213bb1ed15f56969bb712fb1249d4465ea8
Nafies Luthfi <nafiesl@gmail.com>
2025-02-26 16:55:04 +0800

    feat: Create new component for public books summary

--- resources/views/guest/books/_show_book_report.blade.php
+++ resources/views/guest/books/_show_book_report.blade.php
@@ -1,4 +1,4 @@
-@livewire('books.financial-summary', ['bookId' => $book->id])
+@livewire('public-books.financial-summary', ['bookId' => $book->id])
 <a
     href="{{ route('public_reports.index', ['active_book_id' => $book->id, 'nonce' => $book->nonce]) }}"
     class="btn bm-btn btn-outline-cyan mt-3"
