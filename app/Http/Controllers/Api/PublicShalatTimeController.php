7e7676a40497264a986dcc5fa1e18e7075dd2e99
Nafies Luthfi <nafiesl@gmail.com>
2025-02-27 22:29:37 +0800

    feat: Add MyQuranShalatTimeService

--- app/Http/Controllers/Api/PublicShalatTimeController.php
+++ app/Http/Controllers/Api/PublicShalatTimeController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\ShalatTimes\ShalatTimeService;
 use Carbon\Carbon;
+use Exception;
 use Facades\App\Helpers\Setting;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -13,80 +14,22 @@ class PublicShalatTimeController extends Controller
 {
     public function show()
     {
-        $cityName = Setting::get('masjid_city_name');
-        $cities = $this->getAvailableCities();
-        $cityId = array_search(strtolower($cityName), $cities);
-        if ($cityId === false) {
-            return response()->json([
-                'error' => 'City not found.',
-            ], 404);
+        $shalatTimeService = app(ShalatTimeService::class);
+        if (!$shalatTimeService) {
+            Log::error('No Shalat Time provider configured.');
+
+            return response()->json(['error' => 'No Shalat Time provider configured.'], 400);
         }
 
         try {
+            $cityName = Setting::get('masjid_city_name');
             $date = Carbon::now()->format('Y-m-d');
-            $shalatTimeProviderKey = config('shalat_time.default_provider');
-            $baseUrl = config('shalat_time.providers.'.$shalatTimeProviderKey.'.api_base_url');
-            $jadwalResponse = Http::get($baseUrl."/sholat/jadwal/{$cityId}/{$date}");
-            $responseData = $this->parseResponseData($jadwalResponse->json());
 
-            return $responseData;
-        } catch (\Exception $e) {
+            return $shalatTimeService->getSchedule($cityName, $date);
+        } catch (Exception $e) {
             Log::error('Error fetching prayer times: '.$e->getMessage());
+
             return response()->json(['error' => 'Failed to fetch prayer times.'], 500);
         }
-    }
-
-    private function getAvailableCities()
-    {
-        $cacheKey = str_replace('\\', '', __METHOD__);
-        if (Cache::has($cacheKey)) {
-            return Cache::get($cacheKey);
-        }
-        $shalatTimeProviderKey = config('shalat_time.default_provider');
-        $baseUrl = config('shalat_time.providers.'.$shalatTimeProviderKey.'.api_base_url');
-        $response = Http::get($baseUrl.'/sholat/kota/semua');
-        if ($response->successful()) {
-            $responseData = $response->json()['data'];
-
-            $cities = [];
-            foreach ($responseData as $city) {
-                $cities[$city['id']] = strtolower($city['lokasi']);
-            }
-        } else {
-            Log::error('Error fetching cities: '.$response->status().' '.$response->body());
-            $cities = [];
-        }
-        Cache::put($cacheKey, $cities, now()->addDay());
-
-        return $cities;
-    }
-
-    private function parseResponseData(array $responseData): array
-    {
-        $schedules = $responseData['data']['jadwal'];
-        if (array_key_exists('subuh', $schedules)) {
-            $schedules['fajr'] = $schedules['subuh'];
-            unset($schedules['subuh']);
-        }
-        if (array_key_exists('terbit', $schedules)) {
-            $schedules['sunrise'] = $schedules['terbit'];
-            unset($schedules['terbit']);
-        }
-        if (array_key_exists('dzuhur', $schedules)) {
-            $schedules['dzuhr'] = $schedules['dzuhur'];
-            unset($schedules['dzuhur']);
-        }
-        if (array_key_exists('ashar', $schedules)) {
-            $schedules['ashr'] = $schedules['ashar'];
-            unset($schedules['ashar']);
-        }
-        if (array_key_exists('tanggal', $schedules)) {
-            $schedules['date_string'] = $schedules['tanggal'];
-            unset($schedules['tanggal']);
-        }
-        asort($schedules);
-        $responseData['data']['jadwal'] = $schedules;
-
-        return $responseData['data'];
     }
 }
