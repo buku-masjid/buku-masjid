5e4ae5a04cc8f9b6202102f3f612246211f5fd61
Nafies Luthfi <nafiesl@gmail.com>
2025-05-12 11:50:55 +0800

    feat: Remove unused off-canvas navigation part

--- resources/views/public_reports/finance/_book_navigation.blade.php
+++ resources/views/public_reports/finance/_book_navigation.blade.php
@@ -13,20 +13,12 @@
         <div class="row justify-content-center gap-2">
             @forelse ($books as $bookItem)
                 <div class="col-md-3 btn-group">
-                    <a href="{{ route('public_reports.index', ['active_book_id' => $bookItem->id, 'nonce' => $bookItem->nonce]) }}" id="show-book-{{ $bookItem->id }}" class="btn {{ $bookItem->id == $selectedBook->id ? 'bm-bg-primary text-light' : 'bm-txt-primary' }}" style="border-top-left-radius: 9px;border-bottom-left-radius: 9px">
+                    <a href="{{ route('public_reports.index', ['active_book_id' => $bookItem->id, 'nonce' => $bookItem->nonce]) }}" id="show-book-{{ $bookItem->id }}" class="btn {{ $bookItem->id == $selectedBook->id ? 'bm-bg-primary text-light' : 'bm-txt-primary' }}" style="border-radius: 9px">
                         <i class="ti ti-book-2 fs-2"></i>&nbsp;{{ $bookItem->name }}
                         @if ($bookItem->id == $selectedBook->id)
                             &nbsp;<span class="ti float-end">&#xea5e;</span>
                         @endif
                     </a>
-                    <button type="button" class="btn {{ $bookItem->id == $selectedBook->id ? 'bm-bg-primary text-light' : 'bm-txt-primary' }} dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" style="max-width: 2.5em;border-top-right-radius: 9px;border-bottom-right-radius: 9px">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="{{ route('public_reports.finance.summary', ['active_book_id' => $bookItem->id, 'nonce' => $bookItem->nonce] + Request::all()) }}"><i class="ti ti-home"></i>&nbsp;{{ __('report.'.$bookItem->report_periode_code) }}</a></li>
-                        <li><a class="dropdown-item" href="{{ route('public_reports.finance.categorized', ['active_book_id' => $bookItem->id, 'nonce' => $bookItem->nonce] + Request::all()) }}"><i class="ti ti-package"></i>&nbsp;{{ __('report.finance_categorized') }}</a></li>
-                        <li><a class="dropdown-item" href="{{ route('public_reports.finance.detailed', ['active_book_id' => $bookItem->id, 'nonce' => $bookItem->nonce] + Request::all()) }}"> <i class="ti ti-alert-circle"></i>&nbsp;{{ __('report.finance_detailed') }}</a></li>
-                    </ul>
                 </div>
             @empty
                 {{ __('book.not_found') }}
