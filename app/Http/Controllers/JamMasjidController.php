3682d1b0edc1e63cfcf9cc90fdd3effe1cfe5093
Erwin <erwins@gmail.com>
2025-05-30 23:46:19 +0700

    Fix linter error 1

--- app/Http/Controllers/JamMasjidController.php
+++ app/Http/Controllers/JamMasjidController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 class JamMasjidController extends Controller
 {
     public function index()
     {
         $theme = env('JAMMASJID_THEME', 'default');
+
         return view("jammasjid.themes.$theme.index");
     }
-} 
+}
