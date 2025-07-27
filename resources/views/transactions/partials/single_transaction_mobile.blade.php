b246a333dd90dae1697b43825c9d9247b59b1021
Nafies Luthfi <nafiesl@gmail.com>
2025-07-28 00:11:58 +0800

    feat: Duplicate button on transaction index

--- resources/views/transactions/partials/single_transaction_mobile.blade.php
+++ resources/views/transactions/partials/single_transaction_mobile.blade.php
@@ -20,6 +20,18 @@
             @endcan
         @endcan
         {{ link_to_route('transactions.show', __('app.detail'), $transaction) }}
+        @can('create', new App\Transaction)
+            | {{ link_to_route(
+                'transactions.create',
+                __('app.duplicate'),
+                [
+                    'action' => $transaction->in_out ? 'add-income' : 'add-spending',
+                    'original_transaction_id' => $transaction->id,
+                    'reference_page' => 'transactions',
+                ] + request(['month', 'year', 'query', 'category_id', 'bank_account_id']),
+                ['id' => 'duplicate-transaction-'.$transaction->id]
+            ) }}
+        @endcan
     </span>
 </div>
 <div style="margin-bottom: 6px;">
