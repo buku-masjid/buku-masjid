4fedc1b2370e1667faa622b98eda38c0ec6c521e
Nafies Luthfi <nafiesl@gmail.com>
2025-09-04 07:02:34 +0800

    docs: Update join link

--- resources/views/layouts/public/_cta_join.blade.php
+++ resources/views/layouts/public/_cta_join.blade.php
@@ -1,4 +1,4 @@
 <div class="col-auto cta-join mt-sm-3">
     Ingin kelola finansial masjid Anda ? <br>
-    <span><a href="https://wa.me/6281548004800">Gabung ke Buku Masjid</a></span>
+    <span><a href="https://bukumasjid.com">Gabung ke Buku Masjid</a></span>
 </div>
